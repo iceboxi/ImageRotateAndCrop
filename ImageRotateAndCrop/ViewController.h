d5e3744c773c7a8b4bb28765c9ecda29cedcefa1
iceboxi <iceboxi@gmail.com>
2015-06-11 20:23:54 +0800

    Initial Commit

---
+++ ImageRotateAndCrop/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ImageRotateAndCrop
+//
+//  Created by iceboxi on 2015/6/11.
+//  Copyright (c) 2015年 iceboxi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
